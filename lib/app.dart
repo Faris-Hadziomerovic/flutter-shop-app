c8a31800ac0dad6125b0cf82da0aad49cd5cbd84
Faris-Hadziomerovic <faris.hadziomerovic@edu.fit.ba>
2022-12-20 20:04:56 +0100

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Shop app'),
+        ),
+        body: const Center(
+          child: Text('Something goes here'),
+        ),
+      ),
+    );
+  }
+}
